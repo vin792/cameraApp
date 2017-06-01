2a11b1bd3d13bce243724e0ad85fa7ff8d8df7c1
Jordan-Weatherford <jrw821@gmail.com>
2017-06-01 16:15:57 -0700

    Added AR function!

--- cameraTest/MonocleViewController.swift
+++ cameraTest/MonocleViewController.swift
@@ -19,6 +19,8 @@ class MonocleViewController: UIViewController {
     let alamoFire = AlamofireService()
     var userLocation: CLLocation?
     var annotations = Array<ARAnnotation>()
+    fileprivate var arViewController: ARViewController!
+
     
     //IBOutlets
     @IBOutlet weak var mapView: MKMapView!
@@ -40,7 +42,20 @@ class MonocleViewController: UIViewController {
                 }
             })
         }
+        showAR()
     }
+    
+    // show AR view
+    func showAR(){
+        arViewController = ARViewController()
+        arViewController.dataSource = self as? ARDataSource
+        arViewController.setAnnotations(annotations)
+        arViewController.presenter.maxVisibleAnnotations = 30
+//        arViewController.headingSmoothingFactor = 0.05
+        self.present(arViewController, animated: true, completion: nil)
+        
+    }
+    
     
     //IBAction - return to camera
     @IBAction func showCameraViewController(_ sender: Any) {
@@ -83,6 +98,21 @@ extension MonocleViewController: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("hit the fail method")
+    }
+}
+
+extension MonocleViewController: ARDataSource {
+    func ar(_ arViewController: ARViewController, viewForAnnotation: ARAnnotation) -> ARAnnotationView {
+        let annotationView = AnnotationView()
+        annotationView.annotation = viewForAnnotation
+        annotationView.delegate = self as? AnnotationViewDelegate
+        annotationView.frame = CGRect(x: 0, y: 0, width: 150, height: 50)
+        
+        return annotationView
+    }
+    
+    func didTouch(annotationView: AnnotationView) {
+        print("Tapped view for POI: \(annotationView.titleLabel?.text)")
     }
 }
 
